b692977c2993f5ff5fcab78b640e9e928c3d498c
Esteban Marín <estebanamm31@gmail.com>
2022-07-10 19:50:32 -0600

    Se suben los archivos del componente principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
